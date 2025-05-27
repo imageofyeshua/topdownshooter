baf78c99bad71c3e5ca40a129a461e9457c230b9
Daniel Park <intheimageofyeshua@gmail.com>
2025-05-27 13:31:04 +0900

    Update RotateTowards Method

--- Source/Engine/Globals.cs
+++ Source/Engine/Globals.cs
@@ -30,58 +30,6 @@ public class Globals
 
     public static float RotateTowards(Vector2 Pos, Vector2 focus)
     {
-
-        float h, sineTheta, angle;
-        if (Pos.Y - focus.Y != 0)
-        {
-            h = (float)Math.Sqrt(Math.Pow(Pos.X - focus.X, 2) + Math.Pow(Pos.Y - focus.Y, 2));
-            sineTheta = (float)(Math.Abs(Pos.Y - focus.Y) / h); //* ((item.Pos.Y-focus.Y)/(Math.Abs(item.Pos.Y-focus.Y))));
-        }
-        else
-        {
-            h = Pos.X - focus.X;
-            sineTheta = 0;
-        }
-
-        angle = (float)Math.Asin(sineTheta);
-
-        // Drawing diagonial lines here.
-        // Quadrant 2
-        if (Pos.X - focus.X > 0 && Pos.Y - focus.Y > 0)
-        {
-            angle = (float)(Math.PI * 3 / 2 + angle);
-        }
-        // Quadrant 3
-        else if (Pos.X - focus.X > 0 && Pos.Y - focus.Y < 0)
-        {
-            angle = (float)(Math.PI * 3 / 2 - angle);
-        }
-        // Quadrant 1
-        else if (Pos.X - focus.X < 0 && Pos.Y - focus.Y > 0)
-        {
-            angle = (float)(Math.PI / 2 - angle);
-        }
-        else if (Pos.X - focus.X < 0 && Pos.Y - focus.Y < 0)
-        {
-            angle = (float)(Math.PI / 2 + angle);
-        }
-        else if (Pos.X - focus.X > 0 && Pos.Y - focus.Y == 0)
-        {
-            angle = (float)Math.PI * 3 / 2;
-        }
-        else if (Pos.X - focus.X < 0 && Pos.Y - focus.Y == 0)
-        {
-            angle = (float)Math.PI / 2;
-        }
-        else if (Pos.X - focus.X == 0 && Pos.Y - focus.Y > 0)
-        {
-            angle = (float)0;
-        }
-        else if (Pos.X - focus.X == 0 && Pos.Y - focus.Y < 0)
-        {
-            angle = (float)Math.PI;
-        }
-
-        return angle;
+        return (float)Math.Atan2(Pos.Y - focus.Y, Pos.X - focus.X) - MathHelper.PiOver2;
     }
 }
