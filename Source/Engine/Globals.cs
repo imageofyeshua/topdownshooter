2692592dd49089e1b69c611ce996a3a38b048d41
Daniel Park <intheimageofyeshua@gmail.com>
2025-05-27 09:53:05 +0900

    Initial Commit

---
+++ Source/Engine/Globals.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+
+namespace TopDownShooter;
+
+public class Globals
+{
+    public static ContentManager content;
+    public static SpriteBatch spriteBatch;
+}
